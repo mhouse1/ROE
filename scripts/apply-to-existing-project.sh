584d0567095678ff8af8a4f6aa3ff4c755d69ecc
mhouse_impulse <michaelhousera@gmail.com>
2026-04-12 03:45:43 -0400

    wip

---
+++ scripts/apply-to-existing-project.sh
@@ -0,0 +1,13 @@
+# This is still WIP, but the idea is to have a script that can apply ROE 
+# scaffolding to an existing project, without overwriting existing files or making 
+# invasive changes. This would be useful for projects that want to adopt ROE after 
+# they've already started, or for projects that want to use some of ROE's features 
+# without fully committing to the whole structure.
+
+# when applying to an existing project we need to be careful not to be invasive
+# ideas: 
+# if the project is using an older version of ROE, we can make some assumptions
+
+# if the project doesnt already use ROE, maybe ask for user confirmation before 
+# applying changes, and be extra careful to not mess with existing files
+# maybe we can also have a "dry run" mode that shows what changes would be made
